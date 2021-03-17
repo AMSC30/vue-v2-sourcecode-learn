43c15ca4fc1f67031fa21b09c7e46662e5b0c3bf
zhengxixuan <930042468@qq.com>
2021-03-17 20:41:53 +0800

    watcher梳理

--- src/core/observer/scheduler.js
+++ src/core/observer/scheduler.js
@@ -4,13 +4,7 @@ import type Watcher from './watcher'
 import config from '../config'
 import { callHook, activateChildComponent } from '../instance/lifecycle'
 
-import {
-  warn,
-  nextTick,
-  devtools,
-  inBrowser,
-  isIE
-} from '../util/index'
+import { warn, nextTick, devtools, inBrowser, isIE } from '../util/index'
 
 export const MAX_UPDATE_COUNT = 100
 
@@ -22,169 +16,112 @@ let waiting = false
 let flushing = false
 let index = 0
 
-/**
- * Reset the scheduler's state.
- */
-function resetSchedulerState () {
-  index = queue.length = activatedChildren.length = 0
-  has = {}
-  if (process.env.NODE_ENV !== 'production') {
-    circular = {}
-  }
-  waiting = flushing = false
+function resetSchedulerState() {
+    index = queue.length = activatedChildren.length = 0
+    has = {}
+    waiting = flushing = false
 }
 
-// Async edge case #6566 requires saving the timestamp when event listeners are
-// attached. However, calling performance.now() has a perf overhead especially
-// if the page has thousands of event listeners. Instead, we take a timestamp
-// every time the scheduler flushes and use that for all event listeners
-// attached during that flush.
 export let currentFlushTimestamp = 0
 
-// Async edge case fix requires storing an event listener's attach timestamp.
 let getNow: () => number = Date.now
 
-// Determine what event timestamp the browser is using. Annoyingly, the
-// timestamp can either be hi-res (relative to page load) or low-res
-// (relative to UNIX epoch), so in order to compare time we have to use the
-// same timestamp type when saving the flush timestamp.
-// All IE versions use low-res event timestamps, and have problematic clock
-// implementations (#9632)
 if (inBrowser && !isIE) {
-  const performance = window.performance
-  if (
-    performance &&
-    typeof performance.now === 'function' &&
-    getNow() > document.createEvent('Event').timeStamp
-  ) {
-    // if the event timestamp, although evaluated AFTER the Date.now(), is
-    // smaller than it, it means the event is using a hi-res timestamp,
-    // and we need to use the hi-res version for event listener timestamps as
-    // well.
-    getNow = () => performance.now()
-  }
+    const performance = window.performance
+    if (
+        performance &&
+        typeof performance.now === 'function' &&
+        getNow() > document.createEvent('Event').timeStamp
+    ) {
+        getNow = () => performance.now()
+    }
 }
 
-/**
- * Flush both queues and run the watchers.
- */
-function flushSchedulerQueue () {
-  currentFlushTimestamp = getNow()
-  flushing = true
-  let watcher, id
+// 将队列当中的每个watcher进行更新
+function flushSchedulerQueue() {
+    currentFlushTimestamp = getNow()
+    // 更新标识
+    flushing = true
+    let watcher, id
 
-  // Sort queue before flush.
-  // This ensures that:
-  // 1. Components are updated from parent to child. (because parent is always
-  //    created before the child)
-  // 2. A component's user watchers are run before its render watcher (because
-  //    user watchers are created before the render watcher)
-  // 3. If a component is destroyed during a parent component's watcher run,
-  //    its watchers can be skipped.
-  queue.sort((a, b) => a.id - b.id)
+    // 在队列中先根据id对watcher进行排序
+    queue.sort((a, b) => a.id - b.id)
 
-  // do not cache length because more watchers might be pushed
-  // as we run existing watchers
-  for (index = 0; index < queue.length; index++) {
-    watcher = queue[index]
-    if (watcher.before) {
-      watcher.before()
+    // 执行watcher中的run方法，在此之前，如果在初始化的时候，options中传入了before，先执行before方法
+    for (index = 0; index < queue.length; index++) {
+        watcher = queue[index]
+        if (watcher.before) {
+            watcher.before()
+        }
+        id = watcher.id
+
+        // watcher执行更新后清空记录
+        has[id] = null
+
+        // 执行run方法，1.视图更新，2.依赖收集，3.执行回调
+        watcher.run()
     }
-    id = watcher.id
-    has[id] = null
-    watcher.run()
-    // in dev build, check and stop circular updates.
-    if (process.env.NODE_ENV !== 'production' && has[id] != null) {
-      circular[id] = (circular[id] || 0) + 1
-      if (circular[id] > MAX_UPDATE_COUNT) {
-        warn(
-          'You may have an infinite update loop ' + (
-            watcher.user
-              ? `in watcher with expression "${watcher.expression}"`
-              : `in a component render function.`
-          ),
-          watcher.vm
-        )
-        break
-      }
+
+    const activatedQueue = activatedChildren.slice()
+    const updatedQueue = queue.slice()
+
+    // 清空队列
+    resetSchedulerState()
+
+    callActivatedHooks(activatedQueue)
+    callUpdatedHooks(updatedQueue)
+
+    if (devtools && config.devtools) {
+        devtools.emit('flush')
     }
-  }
-
-  // keep copies of post queues before resetting state
-  const activatedQueue = activatedChildren.slice()
-  const updatedQueue = queue.slice()
-
-  resetSchedulerState()
-
-  // call component updated and activated hooks
-  callActivatedHooks(activatedQueue)
-  callUpdatedHooks(updatedQueue)
-
-  // devtool hook
-  /* istanbul ignore if */
-  if (devtools && config.devtools) {
-    devtools.emit('flush')
-  }
 }
 
-function callUpdatedHooks (queue) {
-  let i = queue.length
-  while (i--) {
-    const watcher = queue[i]
-    const vm = watcher.vm
-    if (vm._watcher === watcher && vm._isMounted && !vm._isDestroyed) {
-      callHook(vm, 'updated')
+function callUpdatedHooks(queue) {
+    let i = queue.length
+    while (i--) {
+        const watcher = queue[i]
+        const vm = watcher.vm
+        if (vm._watcher === watcher && vm._isMounted && !vm._isDestroyed) {
+            callHook(vm, 'updated')
+        }
     }
-  }
 }
 
-/**
- * Queue a kept-alive component that was activated during patch.
- * The queue will be processed after the entire tree has been patched.
- */
-export function queueActivatedComponent (vm: Component) {
-  // setting _inactive to false here so that a render function can
-  // rely on checking whether it's in an inactive tree (e.g. router-view)
-  vm._inactive = false
-  activatedChildren.push(vm)
+export function queueActivatedComponent(vm: Component) {
+    vm._inactive = false
+    activatedChildren.push(vm)
 }
 
-function callActivatedHooks (queue) {
-  for (let i = 0; i < queue.length; i++) {
-    queue[i]._inactive = true
-    activateChildComponent(queue[i], true /* true */)
-  }
+function callActivatedHooks(queue) {
+    for (let i = 0; i < queue.length; i++) {
+        queue[i]._inactive = true
+        activateChildComponent(queue[i], true)
+    }
 }
 
-/**
- * Push a watcher into the watcher queue.
- * Jobs with duplicate IDs will be skipped unless it's
- * pushed when the queue is being flushed.
- */
-export function queueWatcher (watcher: Watcher) {
-  const id = watcher.id
-  if (has[id] == null) {
-    has[id] = true
-    if (!flushing) {
-      queue.push(watcher)
-    } else {
-      // if already flushing, splice the watcher based on its id
-      // if already past its id, it will be run next immediately.
-      let i = queue.length - 1
-      while (i > index && queue[i].id > watcher.id) {
-        i--
-      }
-      queue.splice(i + 1, 0, watcher)
-    }
-    // queue the flush
-    if (!waiting) {
-      waiting = true
+export function queueWatcher(watcher: Watcher) {
+    // 将watcher放入队列当中
+    const id = watcher.id
+    if (has[id] == null) {
+        has[id] = true
+        // 在将watcher加入队列时，如果当前正在进行更新，根据当前id将watcher插入队列当中
+        // 否则直接将watcher放入队尾，因为在后期进行真正更新的时候对队列进行sort排序
+        if (!flushing) {
+            queue.push(watcher)
+        } else {
+            let i = queue.length - 1
+            while (i > index && queue[i].id > watcher.id) {
+                i--
+            }
+            queue.splice(i + 1, 0, watcher)
+        }
 
-      if (process.env.NODE_ENV !== 'production' && !config.async) {
-        flushSchedulerQueue()
-        return
-      }
-      nextTick(flushSchedulerQueue)
+        // 之前没有watcher放入队列，将watcher的执行放在下一个事件循环当中
+        // 如果之前放过watcher并且还没有结束，直接放入队列中
+        if (!waiting) {
+            waiting = true
+
+            nextTick(flushSchedulerQueue)
+        }
     }
-  }
 }
--- src/core/observer/watcher.js
+++ src/core/observer/watcher.js
@@ -16,24 +16,6 @@ let uid = 0
  * This is used for both the $watch() api and directives.
  */
 export default class Watcher {
-    vm: Component
-    expression: string
-    cb: Function
-    id: number
-    deep: boolean
-    user: boolean
-    lazy: boolean
-    sync: boolean
-    dirty: boolean
-    active: boolean
-    deps: Array<Dep>
-    newDeps: Array<Dep>
-    depIds: SimpleSet
-    newDepIds: SimpleSet
-    before: ?Function
-    getter: Function
-    value: any
-
     constructor(
         vm: Component,
         expOrFn: string | Function,
@@ -64,9 +46,9 @@ export default class Watcher {
         }
         // 挂载依赖数据变化时的回调函数
         this.cb = cb
-        this.id = ++uid // uid for batching
+        this.id = ++uid
         this.active = true
-        this.dirty = this.lazy // for lazy watchers
+        this.dirty = this.lazy
 
         // 存放依赖收集器
         this.deps = []
@@ -77,31 +59,14 @@ export default class Watcher {
         // 初始化取值表达式
         this.expression = process.env.NODE_ENV !== 'production' ? expOrFn.toString() : ''
 
-        if (typeof expOrFn === 'function') {
-            // 如果初始化watcher实例时传入的表达式是一个函数，直接赋给watcher的getter属性
-            this.getter = expOrFn
-        } else {
-            // 如果传入的是一个取值路径，根据路径生成getter函数
-            this.getter = parsePath(expOrFn)
-            if (!this.getter) {
-                this.getter = noop
-                process.env.NODE_ENV !== 'production' &&
-                    warn(
-                        `Failed watching path: "${expOrFn}" ` +
-                            'Watcher only accepts simple dot-delimited paths. ' +
-                            'For full control, use a function instead.',
-                        vm
-                    )
-            }
-        }
+        // 如果初始化watcher实例时传入的表达式是一个函数，直接赋给watcher的getter属性
+        // 如果传入的是一个取值路径，根据路径生成getter函数
+        this.getter = typeof expOrFn === 'function' ? expOrFn : parsePath(expOrFn)
 
         // 调用get方法取值
         this.value = this.lazy ? undefined : this.get()
     }
 
-    /**
-     * Evaluate the getter, and re-collect dependencies.
-     */
     get() {
         // 将当前实例挂载到Dep的target上面
         pushTarget(this)
@@ -109,6 +74,9 @@ export default class Watcher {
         const vm = this.vm
         // 根据取值函数获取值
         try {
+            // 此方法有两个作用
+            // 一个是进行DOM更新
+            // 一个是进行依赖收集
             value = this.getter.call(vm, vm)
         } catch (e) {
             if (this.user) {
@@ -117,42 +85,41 @@ export default class Watcher {
                 throw e
             }
         } finally {
-            // "touch" every property so they are all tracked as
-            // dependencies for deep watching
             if (this.deep) {
+                // 进行深度监听，通过访问值的方式，将当前watcher添加到依赖收集器当中
                 traverse(value)
             }
             popTarget()
+            //此时依赖收集已经完成 处理上一轮收集器的依赖项 替换收集器合集
             this.cleanupDeps()
         }
         return value
     }
 
-    /**
-     * Add a dependency to this directive.
-     */
     addDep(dep: Dep) {
         const id = dep.id
         if (!this.newDepIds.has(id)) {
+            // 如果在新的收集器合集中没有当前dep，将这个dep放进新的合集中
             this.newDepIds.add(id)
             this.newDeps.push(dep)
+            // 如果当前的watcher在上一轮的依赖收集中没有使用到这个数据，将这个watcher添加到这个数据dep上
             if (!this.depIds.has(id)) {
                 dep.addSub(this)
             }
         }
     }
 
-    /**
-     * Clean up for dependency collection.
-     */
     cleanupDeps() {
         let i = this.deps.length
         while (i--) {
             const dep = this.deps[i]
+            // 如果上一轮的依赖收集器合集中不存在这次数据更新后的的收集器合集中，说明当前的watcher没有依赖这个收集器对应的数据
+            // 将这个watcher从数据的依赖收集器中删除，数据更新时不再需要通知这个watcher
             if (!this.newDepIds.has(dep.id)) {
                 dep.removeSub(this)
             }
         }
+        // 收集结束后，将新的收集器合集把上一轮的合集替换掉
         let tmp = this.depIds
         this.depIds = this.newDepIds
         this.newDepIds = tmp
@@ -163,39 +130,30 @@ export default class Watcher {
         this.newDeps.length = 0
     }
 
-    /**
-     * Subscriber interface.
-     * Will be called when a dependency changes.
-     */
     update() {
-        /* istanbul ignore else */
         if (this.lazy) {
             this.dirty = true
         } else if (this.sync) {
+            // 如果是同步，则直接执行回调
             this.run()
         } else {
+            // 异步执行回调，将watcher放入队列中
             queueWatcher(this)
         }
     }
 
-    /**
-     * Scheduler job interface.
-     * Will be called by the scheduler.
-     */
     run() {
         if (this.active) {
+            // 获取最新的值，此时又会进行依赖收集
+            // 所以说，进行依赖收集有两个地方
+            // 一个是在watcher进行初始化的时候
+            // 一个是在依赖的数据发生变化的时候
             const value = this.get()
-            if (
-                value !== this.value ||
-                // Deep watchers and watchers on Object/Arrays should fire even
-                // when the value is the same, because the value may
-                // have mutated.
-                isObject(value) ||
-                this.deep
-            ) {
-                // set new value
+
+            if (value !== this.value || isObject(value) || this.deep) {
                 const oldValue = this.value
                 this.value = value
+                // 执行回调函数
                 if (this.user) {
                     try {
                         this.cb.call(this.vm, value, oldValue)
@@ -209,18 +167,11 @@ export default class Watcher {
         }
     }
 
-    /**
-     * Evaluate the value of the watcher.
-     * This only gets called for lazy watchers.
-     */
     evaluate() {
         this.value = this.get()
         this.dirty = false
     }
 
-    /**
-     * Depend on all deps collected by this watcher.
-     */
     depend() {
         let i = this.deps.length
         while (i--) {
@@ -228,9 +179,6 @@ export default class Watcher {
         }
     }
 
-    /**
-     * Remove self from all dependencies' subscriber list.
-     */
     teardown() {
         if (this.active) {
             // remove self from vm's watcher list
