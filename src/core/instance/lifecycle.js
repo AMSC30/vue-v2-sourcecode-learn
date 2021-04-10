b8de805c03ee1db61a249a9f789b87f14d5e82e4
“zhengxixuan” <930042468@qq.com>
2021-04-10 22:06:22 +0800

    删除无用

--- src/core/instance/lifecycle.js
+++ src/core/instance/lifecycle.js
@@ -132,56 +132,15 @@ export function lifecycleMixin(Vue: Class<Component>) {
 
 export function mountComponent(vm: Component, el: ?Element, hydrating?: boolean): Component {
     vm.$el = el
-    if (!vm.$options.render) {
-        vm.$options.render = createEmptyVNode
-        if (process.env.NODE_ENV !== 'production') {
-            /* istanbul ignore if */
-            if (
-                (vm.$options.template && vm.$options.template.charAt(0) !== '#') ||
-                vm.$options.el ||
-                el
-            ) {
-                warn(
-                    'You are using the runtime-only build of Vue where the template ' +
-                        'compiler is not available. Either pre-compile the templates into ' +
-                        'render functions, or use the compiler-included build.',
-                    vm
-                )
-            } else {
-                warn('Failed to mount component: template or render function not defined.', vm)
-            }
-        }
-    }
+
+    !vm.$options.render && (vm.$options.render = createEmptyVNode)
+
     callHook(vm, 'beforeMount')
 
-    let updateComponent
-    /* istanbul ignore if */
-    if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
-        updateComponent = () => {
-            const name = vm._name
-            const id = vm._uid
-            const startTag = `vue-perf-start:${id}`
-            const endTag = `vue-perf-end:${id}`
-
-            mark(startTag)
-            const vnode = vm._render()
-            mark(endTag)
-            measure(`vue ${name} render`, startTag, endTag)
-
-            mark(startTag)
-            vm._update(vnode, hydrating)
-            mark(endTag)
-            measure(`vue ${name} patch`, startTag, endTag)
-        }
-    } else {
-        updateComponent = () => {
-            vm._update(vm._render(), hydrating)
-        }
+    let updateComponent = () => {
+        vm._update(vm._render(), hydrating)
     }
 
-    // we set this to vm._watcher inside the watcher's constructor
-    // since the watcher's initial patch may call $forceUpdate (e.g. inside child
-    // component's mounted hook), which relies on vm._watcher being already defined
     new Watcher(
         vm,
         updateComponent,
--- src/core/instance/render.js
+++ src/core/instance/render.js
@@ -79,40 +79,17 @@ export function renderMixin(Vue: Class<Component>) {
             )
         }
 
-        // set parent vnode. this allows render functions to have access
-        // to the data on the placeholder node.
         vm.$vnode = _parentVnode
-        // render self
-        let vnode
-        try {
-            // There's no need to maintain a stack because all render fns are called
-            // separately from one another. Nested component's render fns are called
-            // when parent component is patched.
-            currentRenderingInstance = vm
-            vnode = render.call(vm._renderProxy, vm.$createElement)
-        } catch (e) {
-            handleError(e, vm, `render`)
-            // return error render result,
-            // or previous vnode to prevent render error causing blank component
-            /* istanbul ignore else */
-            if (process.env.NODE_ENV !== 'production' && vm.$options.renderError) {
-                try {
-                    vnode = vm.$options.renderError.call(vm._renderProxy, vm.$createElement, e)
-                } catch (e) {
-                    handleError(e, vm, `renderError`)
-                    vnode = vm._vnode
-                }
-            } else {
-                vnode = vm._vnode
-            }
-        } finally {
-            currentRenderingInstance = null
-        }
-        // if the returned array contains only a single node, allow it
+
+        currentRenderingInstance = vm
+
+        let vnode = render.call(vm._renderProxy, vm.$createElement)
+
+        currentRenderingInstance = null
+
         if (Array.isArray(vnode) && vnode.length === 1) {
             vnode = vnode[0]
         }
-        // return empty vnode in case the render function errored out
         if (!(vnode instanceof VNode)) {
             if (process.env.NODE_ENV !== 'production' && Array.isArray(vnode)) {
                 warn(
@@ -123,7 +100,7 @@ export function renderMixin(Vue: Class<Component>) {
             }
             vnode = createEmptyVNode()
         }
-        // set parent
+
         vnode.parent = _parentVnode
         return vnode
     }
