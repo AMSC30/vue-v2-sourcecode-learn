9d32559ec60aa2a5e4c5ccc2f5e0e84e30015ffe
zhengxixuan <930042468@qq.com>
2022-07-01 13:45:27 +0800

    reset

--- src/core/index.js
+++ src/core/index.js
@@ -1,27 +1,27 @@
-import Vue from './instance/index'
-import { initGlobalAPI } from './global-api/index'
-import { isServerRendering } from 'core/util/env'
-import { FunctionalRenderContext } from 'core/vdom/create-functional-component'
+import Vue from "./instance/index";
+import { initGlobalAPI } from "./global-api/index";
+import { isServerRendering } from "core/util/env";
+import { FunctionalRenderContext } from "core/vdom/create-functional-component";
 
-initGlobalAPI(Vue)
+initGlobalAPI(Vue);
 
 // vue运行环境判断
-Object.defineProperty(Vue.prototype, '$isServer', {
-    get: isServerRendering
-})
+Object.defineProperty(Vue.prototype, "$isServer", {
+  get: isServerRendering,
+});
 
 // 判断是否是服务端渲染
-Object.defineProperty(Vue.prototype, '$ssrContext', {
-    // getter中的this指向的是属性所在实例
-    get() {
-        return this.$vnode && this.$vnode.ssrContext
-    }
-})
+Object.defineProperty(Vue.prototype, "$ssrContext", {
+  // getter中的this指向的是属性所在实例
+  get() {
+    return this.$vnode && this.$vnode.ssrContext;
+  },
+});
 
-Object.defineProperty(Vue, 'FunctionalRenderContext', {
-    value: FunctionalRenderContext
-})
+Object.defineProperty(Vue, "FunctionalRenderContext", {
+  value: FunctionalRenderContext,
+});
 
-Vue.version = '__VERSION__'
+Vue.version = "__VERSION__";
 
-export default Vue
+export default Vue;
